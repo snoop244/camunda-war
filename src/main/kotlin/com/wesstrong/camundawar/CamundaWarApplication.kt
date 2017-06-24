98243d20c474e87b07065c7e9294a22d5a9308a0
snoop244 <snewport@wesstrong.com>
2017-06-24 16:38:20 -0400

    Initial commit

---
+++ src/main/kotlin/com/wesstrong/camundawar/CamundaWarApplication.kt
@@ -0,0 +1,11 @@
+package com.wesstrong.camundawar
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CamundaWarApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CamundaWarApplication::class.java, *args)
+}
---
+++ src/main/kotlin/com/wesstrong/camundawar/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.wesstrong.camundawar
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+		return application.sources(CamundaWarApplication::class.java)
+	}
+
+}
